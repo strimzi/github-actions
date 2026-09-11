d63a77e6be7c1a52fed64c87f5aa35bde9395cc8
Jakub Stejskal <xstejs24@gmail.com>
2026-09-11 16:37:20 +0200

    Add retries to download commands in installers scripts and always checks versions to ensure it is working (#79)
    * Add retries to download commands in installers scripts and always checks versions to ensure it is working
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    * Use curl for downloading tools
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>
    
    ---------
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- .github/actions/dependencies/install-syft/install_syft.sh
+++ .github/actions/dependencies/install-syft/install_syft.sh
@@ -6,8 +6,9 @@ if [ -z "$ARCH" ]; then
     ARCH="amd64"
 fi
 
-wget https://github.com/anchore/syft/releases/download/v${VERSION}/syft_${VERSION}_linux_${ARCH}.tar.gz -O syft.tar.gz
+curl -fL --retry 3 -o syft.tar.gz "https://github.com/anchore/syft/releases/download/v${VERSION}/syft_${VERSION}_linux_${ARCH}.tar.gz"
 tar xf syft.tar.gz -C /tmp
 rm -f syft.tar.gz
 chmod +x /tmp/syft
 sudo mv /tmp/syft /usr/bin
+syft version
